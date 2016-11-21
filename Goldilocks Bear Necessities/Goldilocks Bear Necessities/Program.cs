411ab770d7ad6c061ebbb696166ea8af8f9b4a35
jmanndev <jmanndev@gmail.com>
2016-11-22 00:34:13 +1100

    Project setup

---
+++ Goldilocks Bear Necessities/Goldilocks Bear Necessities/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goldilocks_Bear_Necessities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
